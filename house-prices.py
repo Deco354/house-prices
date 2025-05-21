d492a56fc99655a822187c207313e7c3fdf0bb2f
Declan McKenna <deco354@gmail.com>
2025-05-21 15:58:37 +0700

    Process features and output

--- house-prices.py
+++ house-prices.py
@@ -1,8 +1,9 @@
 import kaggle
 import pandas as pd
+import matplotlib.pyplot as plt
 from pathlib import Path
 from zipfile import ZipFile
-
+from sklearn.preprocessing import StandardScaler
 
 # Create data directory if it doesn't exist
 data_dir = Path("data")
@@ -21,3 +22,25 @@ with ZipFile(zip_path, "r") as zip_ref:
 train_df = pd.read_csv(data_dir / "train.csv")
 test_df = pd.read_csv(data_dir / "test.csv")
 sample_submission_df = pd.read_csv(data_dir / "sample_submission.csv")
+
+# View Columns and select features and target variable
+
+train_df.columns
+features = ["GrLivArea", "YearBuilt", "FullBath", "BedroomAbvGr", "TotRmsAbvGrd"]
+x = train_df[features]
+train_df.GrLivArea.mean()
+train_df.GrLivArea.std()
+y = train_df.SalePrice
+x.describe()
+
+# Check if data has na values
+x.isna().sum()
+
+# Create histogram of GrLivArea using pandas to check if there are significant outliers
+train_df["GrLivArea"].hist()
+y.hist()
+
+# Scale the features and target variable
+scaler = StandardScaler()
+x_scaled = scaler.fit_transform(x)
+y_scaled = (y - y.mean()) / y.std()
